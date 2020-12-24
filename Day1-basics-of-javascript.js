207cd3aac0a42c3594b9c325092f5160359eb614
SriSravyaN <50413203+SriSravyaN@users.noreply.github.com>
2020-12-24 23:08:45 +0530

    day1 workfile

---
+++ Day1-basics-of-javascript.js
@@ -0,0 +1,29 @@
+// values,types and operators of javascript
+
+//1.values
+
+//arithmetic operators -> + / * - 
+
+a=100 + 4 * 11
+
+b=(100 + 4) * 11
+
+console.log(a)
+console.log(b)
+
+//strings
+
+" "
+' '
+` `
+
+console.log("This is the first line\nAnd this is the second")
+
+\ is escaping 
+\n new line
+\t new tab 
+
+\\ operations on strings-concatenation
+
+console.log("con" + "cat" + "e" + "nate")
+
